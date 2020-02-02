7d3eba9d4ea5f58ae6f8d94d3950aba1feb85beb
Bongmin Shinget config --global user.email bongmin0224@gmail.com <bongmin0224@gmail.com>
2020-02-03 00:32:44 +1300

    rebuild page

---
+++ precache-manifest.2b72bdecdb1c6cdc34ef5198e824c10d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa0faec194c48a681afaa2d6783d626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf8816edb47674a8f34",
+    "url": "/static/css/2.2988b753.chunk.css"
+  },
+  {
+    "revision": "fbfb5052222e476b9b20",
+    "url": "/static/css/main.bacbf9ae.chunk.css"
+  },
+  {
+    "revision": "faf8816edb47674a8f34",
+    "url": "/static/js/2.76f6b11f.chunk.js"
+  },
+  {
+    "revision": "fbfb5052222e476b9b20",
+    "url": "/static/js/main.54add3ca.chunk.js"
+  },
+  {
+    "revision": "7d8d1025a29b0f163892",
+    "url": "/static/js/runtime-main.ba9d0275.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4411bc4d8aa7805766a9b5141e368e7b.js"
+  "/precache-manifest.2b72bdecdb1c6cdc34ef5198e824c10d.js"
 );
 
 self.addEventListener('message', (event) => {
